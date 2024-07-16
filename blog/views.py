aa0da006ffa3e36fa421a932e357cb7de775be06
amahadnezhad <amahadnezhad@gmail.com>
2024-07-16 16:27:22 +0330

    Add PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('home/', HomePageView.as_view(), name='home'),
 
 ]
