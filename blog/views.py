69cf1239feed8affcf784bed242642d5875fba36
amahadnezhad <amahadnezhad@gmail.com>
2024-07-16 16:31:59 +0330

    Add PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,9 @@ class PostListView(generic.ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = 'post'
