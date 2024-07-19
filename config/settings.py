76676cc29e90ae2f2cdb763bf49c68e476859f13
amahadnezhad <amahadnezhad@gmail.com>
2024-07-19 15:32:10 +0330

    Translate _base.html Template

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+LOCALE_PATHS = (
+    'templates/locale',
+)
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
