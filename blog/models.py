0e6bcf53df2eaffe65f2e4d2ec57a090119b1003
amahadnezhad <amahadnezhad@gmail.com>
2024-07-20 16:17:44 +0330

    Translate Post & Comment Model

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('pub', 'Published'),
-        ('drf', 'Draft'),
+        ('pub', _('Published')),
+        ('drf', _('Draft')),
     )
 
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    description = models.TextField()
-    cover = models.ImageField(upload_to='covers/', blank=True)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=3, default=STATUS_CHOICES[0][0])
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('Author'))
+    title = models.CharField(max_length=250, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    cover = models.ImageField(upload_to='covers/', blank=True, verbose_name=_('Cover'))
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3, default=STATUS_CHOICES[0][0],
+                              verbose_name=_('Status'))
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('DateTimeCreated'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('DateTimeModified'))
 
     def __str__(self):
         return self.title
@@ -25,13 +27,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    text = models.TextField()
-    helpful = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=True)
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('User'))
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, verbose_name=_('Post'))
+    text = models.TextField(verbose_name=_('Text'))
+    helpful = models.BooleanField(default=True, verbose_name=_('Post Was helpful?'))
+    is_active = models.BooleanField(default=True, verbose_name=_('Is_active'))
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('DateTimeCreated'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('DateTimeModified'))
 
     def __str__(self):
         return f"{self.user}: {self.text}"
